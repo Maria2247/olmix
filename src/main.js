b28686829cfbd8f1936b44f3b00292400e8132e6
Mariia Savchenko <mashasavchenko@yahoo.com>
2026-05-15 21:07:45 +0300

    added why-us and contacts section

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ createIcons({
 const openMenuBtn = document.querySelector('.menu-btn');
 const mobileMenu = document.querySelector('.main-nav');
 const closeMenuBtn = document.querySelector('.close-btn');
+const navLink = document.querySelectorAll('.nav-item a');
 
 openMenuBtn.addEventListener('click', () => {
   mobileMenu.classList.add('is-open');
@@ -26,65 +27,26 @@ openMenuBtn.addEventListener('click', () => {
   burgerBtn.setAttribute('aria-expanded', !expanded);
 });
 
-closeMenuBtn.addEventListener('click', () => {
+function closeMobileNav() {
   mobileMenu.classList.remove('is-open');
+}
+
+navLink.forEach(link => {
+  link.addEventListener('click', closeMobileNav);
 });
-// import javascriptLogo from './assets/javascript.svg';
-// import viteLogo from './assets/vite.svg';
-// import heroImg from './assets/hero.png';
-// import { setupCounter } from './counter.js';
 
-// document.querySelector('#app').innerHTML = `
-// <section id="center">
-//   <div class="hero">
-//     <img src="${heroImg}" class="base" width="170" height="179">
-//     <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-//     <img src="${viteLogo}" class="vite" alt="Vite logo" />
-//   </div>
-//   <div>
-//     <h1>Get started</h1>
-//     <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-//   </div>
-//   <button id="counter" type="button" class="counter"></button>
-// </section>
+closeMenuBtn.addEventListener('click', closeMobileNav);
 
-// <div class="ticks"></div>
+// FIXED HEADER
 
-// <section id="next-steps">
-//   <div id="docs">
-//     <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-//     <h2>Documentation</h2>
-//     <p>Your questions, answered</p>
-//     <ul>
-//       <li>
-//         <a href="https://vite.dev/" target="_blank">
-//           <img class="logo" src="${viteLogo}" alt="" />
-//           Explore Vite
-//         </a>
-//       </li>
-//       <li>
-//         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//           <img class="button-icon" src="${javascriptLogo}" alt="">
-//           Learn more
-//         </a>
-//       </li>
-//     </ul>
-//   </div>
-//   <div id="social">
-//     <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-//     <h2>Connect with us</h2>
-//     <p>Join the Vite community</p>
-//     <ul>
-//       <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-//       <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-//       <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-//       <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-//     </ul>
-//   </div>
-// </section>
+// const header = document.querySelector('.header-container');
 
-// <div class="ticks"></div>
-// <section id="spacer"></section>
-// `;
+// const stickyPoint = header.offsetTop;
 
-// setupCounter(document.querySelector('#counter'));
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > stickyPoint) {
+//     header.classList.add('is-fixed');
+//   } else {
+//     header.classList.remove('is-fixed');
+//   }
+// });
