8a42c1f83502a898d3cb87434c4c05e262c160d3
Mariia Savchenko <mashasavchenko@yahoo.com>
2026-05-17 23:40:55 +0300

    fix vite.config for gitHub deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
-export default defineConfig({
-  base: './',
-  build: {
-    rolldownOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        english: path.resolve(__dirname, 'en/index.html'),
+export default defineConfig(({ mode }) => {
+  const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
+  const basePrefix = isGithubPages ? '/olmix/' : './';
+  return {
+    base: './',
+    build: {
+      rolldownOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+          english: path.resolve(__dirname, 'en/index.html'),
+        },
       },
+      outDir: 'dist',
+      sourcemap: true,
     },
-    outDir: 'dist',
-    sourcemap: true,
-  },
+  };
 });
